d935abfa0879e0798b8fb3258574564b1d8738fc
Evaldo Gomes Silva <evaldogomes@gmail.com>
2022-04-23 09:18:10 -0300

    First release

---
+++ PowerfulConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+var builder = new ConfigurationBuilder();
+BuildConfig(builder);
+
+var config = builder.Build();
+
+Log.Logger = new LoggerConfiguration().
+    ReadFrom.Configuration(config).
+    Enrich.FromLogContext().
+    WriteTo.Console().
+    CreateLogger();
+
+Log.Logger.Information("App starting");
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) =>
+    {
+        services.AddTransient<IGreetingService, GreetingService>();
+    })
+    .UseSerilog()
+    .Build();
+
+// Not that good way to instatiate in this case, because doesn't use the dependency injection
+// Better way Ex: https://github.com/evaldosilva/ConsoleDependencyInjectionApp/blob/844bef71c3336bb4feaaeb42c67e8b8b534c9149/ConsoleDependencyInjectionApp/Program.cs#L42
+var svc = ActivatorUtilities.CreateInstance<GreetingService>(host.Services);
+svc.Run();
+
+static void BuildConfig(IConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings{Environment.GetEnvironmentVariable("ASPNETCORE>ENVIRONMENT") ?? "Production"}.json",
+        optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables();
+}
